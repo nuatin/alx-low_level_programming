91adb5169dc91a36b4deaa86cdebb47a4db5484e
nuatin <nuating@yahoo.com>
2023-06-16 17:09:29 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - A program that prints a line with put function
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
+}
+}
